af5d03cdac9f9875938866b43037141cb7535260
ShSerg0699 <gambit240699@gmail.com>
2020-06-15 02:41:51 +0700

    working version with autocomplete database

--- src/main/resources/db/changelog/creates/creates.sql
+++ src/main/resources/db/changelog/creates/creates.sql
@@ -1,179 +1,80 @@
 CREATE TABLE public.buyer
 (
-    buyerid integer NOT NULL DEFAULT nextval('buyer_buyerid_seq'::regclass),
-    name character varying(255) COLLATE pg_catalog."default",
-    CONSTRAINT buyer_pkey PRIMARY KEY (buyerid)
-)
+    buyerid integer primary key generated always as identity,
+    name character varying(255)
+);
 
-TABLESPACE pg_default;
-
-ALTER TABLE public.buyer
-    OWNER to postgres;
 
 CREATE TABLE public.cell_in_stock
 (
-    cellid integer NOT NULL DEFAULT nextval('cell_in_stock_cellid_seq'::regclass),
-    space integer NOT NULL,
-    CONSTRAINT cell_in_stock_pkey PRIMARY KEY (cellid)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE public.cell_in_stock
-    OWNER to postgres;
+    cellid integer primary key generated always as identity,
+    space integer NOT NULL
+);
 
 CREATE TABLE public.detail
 (
-    detailid integer NOT NULL DEFAULT nextval('detail_detailid_seq'::regclass),
-    name character varying(255) COLLATE pg_catalog."default",
+    detailid integer primary key generated always as identity,
+    name character varying(255),
     price integer NOT NULL,
-    size integer NOT NULL,
-    CONSTRAINT detail_pkey PRIMARY KEY (detailid)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE public.detail
-    OWNER to postgres;
+    size integer NOT NULL
+);
 
 CREATE TABLE public.cell_detail
 (
-    cell_detailid integer NOT NULL DEFAULT nextval('cell_detail_cell_detailid_seq'::regclass),
+    cell_detailid integer primary key generated always as identity,
     appearance_date date,
     quantity integer NOT NULL,
-    cell_in_stock_cellid integer,
-    detail_detailid integer,
-    CONSTRAINT cell_detail_pkey PRIMARY KEY (cell_detailid),
-    CONSTRAINT fkhof3s765p8ebsfckg35nsdnu2 FOREIGN KEY (detail_detailid)
-        REFERENCES public.detail (detailid) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT fkojlbqohswubadoorrn5lfwh7p FOREIGN KEY (cell_in_stock_cellid)
-        REFERENCES public.cell_in_stock (cellid) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE public.cell_detail
-    OWNER to postgres;
+    cell_in_stock_cellid integer references cell_in_stock,
+    detail_detailid integer references detail
+);
 
 CREATE TABLE public.purchase
 (
-    purchaseid integer NOT NULL DEFAULT nextval('purchase_purchaseid_seq'::regclass),
+    purchaseid integer primary key generated always as identity,
     purchase_date date,
-    buyer_buyerid integer,
-    CONSTRAINT purchase_pkey PRIMARY KEY (purchaseid),
-    CONSTRAINT fkshq37r75rwbvruyxieuje27q6 FOREIGN KEY (buyer_buyerid)
-        REFERENCES public.buyer (buyerid) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE public.purchase
-    OWNER to postgres;
+    buyer_buyerid integer references buyer
+);
 
 CREATE TABLE public.purchase_detail
 (
-    purchase_detailid integer NOT NULL DEFAULT nextval('purchase_detail_purchase_detailid_seq'::regclass),
+    purchase_detailid integer primary key generated always as identity,
     quantity integer NOT NULL,
-    detail_detailid integer,
-    purchase_purchaseid integer,
-    CONSTRAINT purchase_detail_pkey PRIMARY KEY (purchase_detailid),
-    CONSTRAINT fkbngjt88we7w2faa8smje9k8jx FOREIGN KEY (detail_detailid)
-        REFERENCES public.detail (detailid) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT fkkre2gyf5xhwj0gpdmmbmgoyug FOREIGN KEY (purchase_purchaseid)
-        REFERENCES public.purchase (purchaseid) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE public.purchase_detail
-    OWNER to postgres;
+    detail_detailid integer references detail,
+    purchase_purchaseid integer references purchase
+);
 
 CREATE TABLE public.supplier
 (
-    supplierid integer NOT NULL DEFAULT nextval('supplier_supplierid_seq'::regclass),
+    supplierid integer primary key generated always as identity,
     contract bigint,
-    delivery_time character varying(255) COLLATE pg_catalog."default",
+    delivery_time character varying(255),
     discount integer,
-    guarantee character varying(255) COLLATE pg_catalog."default",
-    name character varying(255) COLLATE pg_catalog."default",
-    type integer NOT NULL,
-    CONSTRAINT supplier_pkey PRIMARY KEY (supplierid)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE public.supplier
-    OWNER to postgres;
+    guarantee character varying(255),
+    name character varying(255),
+    type integer NOT NULL
+);
 
 CREATE TABLE public.price_list
 (
-    price_listid integer NOT NULL DEFAULT nextval('price_list_price_listid_seq'::regclass),
+    price_listid integer primary key generated always as identity,
     price integer NOT NULL,
-    detail_detailid integer,
-    supplier_supplierid integer,
-    CONSTRAINT price_list_pkey PRIMARY KEY (price_listid),
-    CONSTRAINT fk3a67n7fp5jhgketl4qiovqkpl FOREIGN KEY (detail_detailid)
-        REFERENCES public.detail (detailid) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT fkb9aaalfcclbbue88u4ny8yb7n FOREIGN KEY (supplier_supplierid)
-        REFERENCES public.supplier (supplierid) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE public.price_list
-    OWNER to postgres;
+    detail_detailid integer references detail,
+    supplier_supplierid integer references supplier
+);
 
 CREATE TABLE public.supply
 (
-    supplyid integer NOT NULL DEFAULT nextval('supply_supplyid_seq'::regclass),
+    supplyid integer primary key generated always as identity,
     customs_clearance integer NOT NULL,
     delivery_date date,
     marriage_rate integer NOT NULL,
-    supplier_supplierid integer,
-    CONSTRAINT supply_pkey PRIMARY KEY (supplyid),
-    CONSTRAINT fkhbngk4dd55b0r9lod98ivtlfu FOREIGN KEY (supplier_supplierid)
-        REFERENCES public.supplier (supplierid) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE public.supply
-    OWNER to postgres;
+    supplier_supplierid integer references supplier
+);
 
 CREATE TABLE public.supply_detail
 (
-    supply_detailid integer NOT NULL DEFAULT nextval('supply_detail_supply_detailid_seq'::regclass),
+    supply_detailid integer primary key generated always as identity,
     quantity integer NOT NULL,
-    detail_detailid integer,
-    supply_supplyid integer,
-    CONSTRAINT supply_detail_pkey PRIMARY KEY (supply_detailid),
-    CONSTRAINT fki3axv9p3ckc1ongo7wshafd7r FOREIGN KEY (supply_supplyid)
-        REFERENCES public.supply (supplyid) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT fkr44lhabqimi4f2h1xevhl0p0e FOREIGN KEY (detail_detailid)
-        REFERENCES public.detail (detailid) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE public.supply_detail
-    OWNER to postgres;
+    detail_detailid integer references  detail,
+    supply_supplyid integer references supply
+);
